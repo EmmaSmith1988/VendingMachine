3c6390de9083ab0048fb56744289c6ba42112247
Emma Smith <smileandsparkle@hotmail.com>
2022-05-25 13:05:30 +0100

    more refactoring of hard coded methods to include the enum and a for loop

--- src/com/nology/vendingmachine/Machine.java
+++ src/com/nology/vendingmachine/Machine.java
@@ -7,65 +7,65 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Machine {
-    int limit = 10;
-    Twix twix = new Twix();
-    DairyMilk dairyMilk = new DairyMilk();
-
-    public int randomNumber(){
-        return (int) Math.floor(Math.random()*(limit+1));
-    }
-
-    public void startup(){
-        twix.setQuantity(this.randomNumber());
-        dairyMilk.setQuantity(this.randomNumber());
-    }
-
-    public void displayStock(){
-        if(twix.getQuantity() == 0){
-            System.out.println("Twix is out of stock");
-        } else {
-            System.out.println("Twix - " + twix.getQuantity());
-        }
-        if(dairyMilk.getQuantity() == 0){
-            System.out.println("Dairy Milk is out of stock");
-        } else {
-            System.out.println("Dairy Milk - " + dairyMilk.getQuantity());
-        }
-    }
-
-    public void takeUserInput(){
-        System.out.println("What would you like to do: Buy? Refill? Check stock? -> ");
-        Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine();
-        if(userInput.equalsIgnoreCase("buy")){
-            this.buy();
-        };
-    }
-
-    public void buy(){
-        System.out.println("Which item would you like to buy? -> ");
-        Scanner scanner = new Scanner(System.in);
-        if(scanner.nextLine().equalsIgnoreCase("dairy milk")){
-            System.out.println("That will be £" + dairyMilk.price + ", please.");
-            System.out.println("How much money are you entering?");
-            double moneyPaid = scanner.nextDouble();
-            if(moneyPaid< dairyMilk.price){
-                System.out.println("That's not enough, please pay £" + (dairyMilk.price-moneyPaid));
-            } else if(moneyPaid== dairyMilk.price) {
-                System.out.println("Thanks, here is your item");
-            } else {
-                System.out.println("Thanks, here is your item and your change - " + (moneyPaid- dairyMilk.price));
-            }
-        };
-    }
-
-    public static void main(String[] args) {
-        Machine machine = new Machine();
-        machine.startup();
-        machine.displayStock();
-        machine.takeUserInput();
-
-
-
-    }
+//    int limit = 10;
+//    Twix twix = new Twix();
+//    DairyMilk dairyMilk = new DairyMilk();
+//
+//    public int randomNumber(){
+//        return (int) Math.floor(Math.random()*(limit+1));
+//    }
+//
+//    public void startup(){
+//        twix.setQuantity(this.randomNumber());
+//        dairyMilk.setQuantity(this.randomNumber());
+//    }
+//
+//    public void displayStock(){
+//        if(twix.getQuantity() == 0){
+//            System.out.println("Twix is out of stock");
+//        } else {
+//            System.out.println("Twix - " + twix.getQuantity());
+//        }
+//        if(dairyMilk.getQuantity() == 0){
+//            System.out.println("Dairy Milk is out of stock");
+//        } else {
+//            System.out.println("Dairy Milk - " + dairyMilk.getQuantity());
+//        }
+//    }
+//
+//    public void takeUserInput(){
+//        System.out.println("What would you like to do: Buy? Refill? Check stock? -> ");
+//        Scanner scanner = new Scanner(System.in);
+//        String userInput = scanner.nextLine();
+//        if(userInput.equalsIgnoreCase("buy")){
+//            this.buy();
+//        };
+//    }
+//
+//    public void buy(){
+//        System.out.println("Which item would you like to buy? -> ");
+//        Scanner scanner = new Scanner(System.in);
+//        if(scanner.nextLine().equalsIgnoreCase("dairy milk")){
+//            System.out.println("That will be £" + dairyMilk.price + ", please.");
+//            System.out.println("How much money are you entering?");
+//            double moneyPaid = scanner.nextDouble();
+//            if(moneyPaid< dairyMilk.price){
+//                System.out.println("That's not enough, please pay £" + (dairyMilk.price-moneyPaid));
+//            } else if(moneyPaid== dairyMilk.price) {
+//                System.out.println("Thanks, here is your item");
+//            } else {
+//                System.out.println("Thanks, here is your item and your change - " + (moneyPaid- dairyMilk.price));
+//            }
+//        };
+//    }
+//
+//    public static void main(String[] args) {
+//        Machine machine = new Machine();
+//        machine.startup();
+//        machine.displayStock();
+//        machine.takeUserInput();
+//
+//
+//
+//    }
 }
--- src/com/nology/vendingmachine/items/VendingMachine.java
+++ src/com/nology/vendingmachine/items/VendingMachine.java
@@ -1,6 +1,7 @@
 package com.nology.vendingmachine.items;
 
 
+import java.util.Scanner;
 
 public class VendingMachine {
 
@@ -8,7 +9,11 @@ public class VendingMachine {
 
     public void displayItems() {
         for(Item item: Item.values()){
-            System.out.println(item.name() + ", price £" + item.getPrice() + ", quantity: " + item.getQuantity());
+            if (item.getQuantity() == 0){
+                System.out.println(item.name() + " is out of stock");
+            } else {
+                System.out.println(item.name() + ", price £" + item.getPrice() + ", quantity: " + item.getQuantity());
+            }
         }
     }
 
@@ -22,11 +27,32 @@ public class VendingMachine {
         }
     }
 
+    public void buy(){
+        System.out.println("Which item would you like to buy? -> ");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        for (Item item: Item.values()) {
+            if(userInput.equalsIgnoreCase(String.valueOf(item))){
+                System.out.println("That will be £" + item.getPrice() + ", please.");
+                System.out.println("How much money are you entering?");
+                double moneyPaid = scanner.nextDouble();
+                if(moneyPaid< item.getPrice()){
+                    System.out.println("That's not enough, please pay £" + (item.getPrice()-moneyPaid));
+                } else if(moneyPaid== item.getPrice()) {
+                    System.out.println("Thanks, here is your item");
+                } else {
+                    System.out.println("Thanks, here is your item and your change - £" + (moneyPaid- item.getPrice()));
+                }
+            }
+        }
+    }
+
 
 
     public static void main(String[] args) {
         VendingMachine vendingMachine = new VendingMachine();
         vendingMachine.startup();
         vendingMachine.displayItems();
+        vendingMachine.buy();
     }
 }
